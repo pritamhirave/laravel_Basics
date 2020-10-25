7f4fbf659f8e19377a68b7a9c96091638ab0a2a7
Pritam Hirve <hiravepritam@gmail.com>
2020-10-25 23:44:19 +0530

    first comit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\usercontroller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+/*
+Route::get('/user/{name}', function ($name) {
+    return view('user',["user"=>$name]);
+});
+*/
+//Route::get("user/{id}",[UserController::class,'show']);
+//Route::get("user",'usercontroller@show');
