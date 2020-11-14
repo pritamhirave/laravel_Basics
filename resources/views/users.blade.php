04e743595dffc1eae69257d082119adb7e3cac43
Pritam Hirve <hiravepritam@gmail.com>
2020-11-15 00:02:08 +0530

    change

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,6 +2,6 @@
 <form action="users" method="get">
     @csrf
     <input type="text" name="username" placeholder="enter user id" /> <br></br>
-    <input type="password" name="userpassword" placeholder="enter user Password" />
+    <input type="password" name="userpassword" placeholder="enter user Password" /><br></br>
     <button type="submit">Login</button><br></br>
 </form>
