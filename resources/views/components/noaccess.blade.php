344ed234faeff0e49c1f852c4e6d5ae686937ca9
Pritam Hirve <hiravepritam@gmail.com>
2020-11-16 23:37:59 +0530

    middleware

---
+++ resources/views/components/noaccess.blade.php
@@ -0,0 +1,1 @@
+<h1>you can not access over website</h1>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,8 +1,1 @@
 <h1>User Login </h1>
-{{$errors}}
-<form action="users" method="get">
-    @csrf
-    <input type="text" name="username" placeholder="enter user id" /> <br></br>
-    <input type="password" name="userpassword" placeholder="enter user Password" /><br></br>
-    <button type="submit">Login</button><br></br>
-</form>
