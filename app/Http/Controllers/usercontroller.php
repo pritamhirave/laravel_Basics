bcb515857f4067df8c1f2dc4aee7ee0dbaa71888
Pritam Hirve <hiravepritam@gmail.com>
2020-11-16 23:20:43 +0530

    change

--- app/Http/Controllers/usercontroller.php
+++ app/Http/Controllers/usercontroller.php
@@ -13,7 +13,6 @@ class usercontroller extends Controller
         {
             $req->validate([
                 'username'=>'required'
-                'userpassword'=>'required'
             ]);
         }
         return $req->input();
